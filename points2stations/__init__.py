ac7845cfa7d7f783cefe8a0a1f43fe80609cfb19
riccardoklinger <riccardo.klinger@geolicious.de>
2016-03-12 11:55:53 +0100

    new projecting function
    added functionality to project points on OSM lines, exporting projected points as geojson.

--- points2stations/__init__.py
+++ points2stations/__init__.py
@@ -28,16 +28,14 @@ def filterPoints(f):
 	with open(f) as file:
 		data = json.load(file)
 	dataF = {}
-	index = 0
 	dataF['type']= 'FeatureCollection'
 	dataF["crs"] = { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } }
 	dataF['features']= []
 	for feature in data['features']:
 		if feature['properties']['speed']==0:
 			dataF['features'].append(feature)
-			index += 1
 	#writing the filtered data to a file:
-	with open('data_filtered.geojson', 'w') as f:
+	with open('data/data_filtered.geojson', 'w') as f:
 		json.dump(dataF, f)
 	print str(index)+" objects written to file"
 	print("executed")
@@ -85,7 +83,8 @@ def simplifyPoints(f,eps,min):
 	plt.xlabel('Lon')
 	plt.ylabel('Lat')
 	plt.show()
-def collect_ways(f, out_file): #get ways for a bounding box imported with a GeoJSON file and store them as a list of points
+	return rs
+def collectWays(f, out_file): #get ways for a bounding box imported with a GeoJSON file and store them as a list of points
 	import json, urllib
 	with open(f) as file:
 		data = json.load(file)
@@ -96,12 +95,45 @@ def collect_ways(f, out_file): #get ways for a bounding box imported with a GeoJ
 		lat.append(feature["geometry"]["coordinates"][1])
 		lon.append(feature["geometry"]["coordinates"][0])
 	bb = [min(lat), min(lon), max(lat), max(lon)]
-	url = 'http://overpass-api.de/api/interpreter?data=[out:json][bbox:' + str(bb).strip('[]') +'];way["highway"];(._;>;);out geom;'
+	url = 'http://overpass-api.de/api/interpreter?data=[out:json][bbox:' + str(bb).strip('[]') +'];way["highway"~"secondary|primary|tertiary"];(._;>;);out geom;'
 	response = urllib.urlopen(url)
 	data2 = json.loads(response.read())
 	with open(out_file, 'w') as outfile:
 		json.dump(data2, outfile)
+def getLinestrings(f): #reads the results from OSM export and returns a MultilineString
+	import json
+	from shapely.geometry import Point, LineString, MultiLineString
+	with open(f) as file:
+		data = json.load(file)
 
+	lines = []
+
+	for elem in data["elements"]:
+		if elem["type"]=="way":
+			line = []
+			for geo in elem["geometry"]:
+				line.append((geo["lon"], geo["lat"]))
+			lines.append(line)
+	lines=MultiLineString(lines)
+	return lines
+def createStation(filteredSetFile,osmFile):
+	import pandas as pd, numpy as np, matplotlib.pyplot as plt	
+	from shapely.geometry import Point
+	lines=getLinestrings(osmFile)
+	rs = simplifyPoints(filteredSetFile, 0.001, 2)
+	rsProjected = []
+	for index, row in rs.iterrows():
+		p = Point(row.lon, row.lat)
+		np = lines.interpolate(lines.project(p))
+		rsProjected.append(np)
+	dataF = {}
+	dataF['type']= 'FeatureCollection'
+	dataF["crs"] = { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } }
+	dataF['features']= []
+	for point in rsProjected:
+		dataF['features'].append({"geometry": {"type": "Point", "coordinates": [point.x, point.y]}, "type": "Feature", "properties": {"previous_dominating_activity": "still", "bearing": 0, "route": "", "previous_dominating_activity_confidence": 60, "current_dominating_activity": "in_vehicle", "current_dominating_activity_confidence": 77, "timestamp": "2015-11-11T09:03:01+0300", "created_at": "2015-11-11 06:03:12", "feature": "passive_tracking", "id": 4, "speed": 0, "altitude": 0.0, "accuracy": 23.0}})
+	with open("data/potential_bus_stations.gojson", 'w') as outfile:
+		json.dump(dataF, outfile)
 def getCentroid(points):
 	import numpy as np
 	n = points.shape[0]
